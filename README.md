0b68849e684099ea91890903bcd213090290db5e
Tom Firth <thomas.d.firth@gmail.com>
2021-11-14 17:12:01 +0000

    Start of part 2

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
 # SER
+
 Software Engineering for Research
 
 ## Installation
+
 ```bash
 conda create -n ser python=3.9
 conda activate ser
 pip install -e .
 ```
 
+## PyTorch dependencies
+
+There are instructions on the PyTorch website how to install the PyTorch versions that are correct for your system...
+
+https://pytorch.org/get-started/locally/#macos-version
+
+The above link assumes you're on a mac... but if not you'll need to select the right options before running the command it gives you.
+
 ## Startup
+
 ```bash
 conda activate ser
-``` 
+```
 
 ## Run SER
+
 ```bash
 ser --help
-``` 
-
+```
--- bin/cli.py
+++ bin/cli.py
@@ -1,11 +1,116 @@
+from pathlib import Path
+import torch
+from torch import optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
 import typer
 
 main = typer.Typer()
 
+PROJECT_ROOT = Path(__file__).parent.parent
+DATA_DIR = PROJECT_ROOT / "data"
+
 
 @main.command()
-def train():
-    print("This is where the training code will go")
+def train(
+    name: str = typer.Option(
+        ..., "-n", "--name", help="Name of experiment to save under."
+    ),
+):
+    print(f"Running experiment {name}")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    epochs = 2
+    batch_size = 1000
+    learning_rate = 0.01
+
+    # save the parameters!
+
+    # load model
+    model = Net().to(device)
+
+    # setup params
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # torch transforms
+    ts = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
+
+    # dataloaders
+    training_dataloader = DataLoader(
+        datasets.MNIST(root="../data", download=True, train=True, transform=ts),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=1,
+    )
+
+    validation_dataloader = DataLoader(
+        datasets.MNIST(root=DATA_DIR, download=True, train=False, transform=ts),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=1,
+    )
+
+    # train
+    for epoch in range(epochs):
+        for i, (images, labels) in enumerate(training_dataloader):
+            images, labels = images.to(device), labels.to(device)
+            model.train()
+            optimizer.zero_grad()
+            output = model(images)
+            loss = F.nll_loss(output, labels)
+            loss.backward()
+            optimizer.step()
+            print(
+                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
+                f"| Loss: {loss.item():.4f}"
+            )
+            # validate
+            val_loss = 0
+            correct = 0
+            with torch.no_grad():
+                for images, labels in validation_dataloader:
+                    images, labels = images.to(device), labels.to(device)
+                    model.eval()
+                    output = model(images)
+                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
+                    pred = output.argmax(dim=1, keepdim=True)
+                    correct += pred.eq(labels.view_as(pred)).sum().item()
+                val_loss /= len(validation_dataloader.dataset)
+                val_acc = correct / len(validation_dataloader.dataset)
+
+                print(
+                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
+                )
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 
 @main.command()
