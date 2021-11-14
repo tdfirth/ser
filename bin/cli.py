28029962b990777e52155d858213cc4dcfc38932
Tom Firth <thomas.d.firth@gmail.com>
2021-11-14 18:01:14 +0000

    Start of part 3

--- bin/cli.py
+++ bin/cli.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from pathlib import Path
 
 import typer
 import torch
@@ -6,7 +7,7 @@ import git
 
 from ser.train import train as run_train
 from ser.constants import RESULTS_DIR
-from ser.data import train_dataloader, val_dataloader
+from ser.data import train_dataloader, val_dataloader, test_dataloader
 from ser.params import Params, save_params
 from ser.transforms import transforms, normalize
 
@@ -59,4 +60,46 @@ def train(
 
 @main.command()
 def infer():
-    print("This is where the inference code will go")
+    run_path = Path("./path/to/one/of/your/training/runs")
+    label = 6
+
+    # TODO load the parameters from the run_path so we can print them out!
+
+    # select image to run inference for
+    dataloader = test_dataloader(1, transforms(normalize))
+    images, labels = next(iter(dataloader))
+    while labels[0].item() != label:
+        images, labels = next(iter(dataloader))
+
+    # load the model
+    model = torch.load(run_path / "model.pt")
+
+    # run inference
+    model.eval()
+    output = model(images)
+    pred = output.argmax(dim=1, keepdim=True)[0].item()
+    certainty = max(list(torch.exp(output)[0]))
+    pixels = images[0][0]
+    print(generate_ascii_art(pixels))
+    print(f"This is a {pred}")
+
+
+def generate_ascii_art(pixels):
+    ascii_art = []
+    for row in pixels:
+        line = []
+        for pixel in row:
+            line.append(pixel_to_char(pixel))
+        ascii_art.append("".join(line))
+    return "\n".join(ascii_art)
+
+
+def pixel_to_char(pixel):
+    if pixel > 0.99:
+        return "O"
+    elif pixel > 0.9:
+        return "o"
+    elif pixel > 0:
+        return "."
+    else:
+        return " "
--- ser/data.py
+++ ser/data.py
@@ -16,3 +16,12 @@ def val_dataloader(batch_size, transforms):
         root=DATA_DIR, download=True, train=False, transform=transforms
     )
     return DataLoader(data, batch_size=batch_size, shuffle=False, num_workers=1)
+
+
+# Returns dataloader with mnist validation data.
+# Note: normally this would be real 'test' or future data
+def test_dataloader(batch_size, transforms):
+    data = datasets.MNIST(
+        root=DATA_DIR, download=True, train=False, transform=transforms
+    )
+    return DataLoader(data, batch_size=batch_size, shuffle=True, num_workers=1)
