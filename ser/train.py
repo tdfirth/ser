2edf40aa43ad656d69b00230830e6430b575cf0b
Ellen V <ellenmvisscher@gmail.com>
2021-11-23 13:16:37 +0000

    Further, split the training function to be modular.

--- ser/train.py
+++ ser/train.py
@@ -1,39 +1,52 @@
 # train
+import numpy as np
 import torch
 import torch.nn.functional as F
 
 from ser.models import Parameters, Data, TrainingModel
 
 
+def train_one_epoch(images: np.array, labels: np.array, training_model: TrainingModel):
+    training_model.model.train()
+    training_model.optimizer.zero_grad()
+    output = training_model.model(images)
+    loss = F.nll_loss(output, labels)
+    loss.backward()
+    training_model.optimizer.step()
+    return loss
+
+
+def get_validation_loss(data: Data, training_model: TrainingModel):
+    # validate
+    val_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for images, labels in data.validation_dataloader:
+            images, labels = images.to(training_model.device), labels.to(training_model.device)
+            training_model.model.eval()
+            output = training_model.model(images)
+            val_loss += F.nll_loss(output, labels, reduction="sum").item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(labels.view_as(pred)).sum().item()
+        val_loss /= len(data.validation_dataloader.dataset)
+        val_acc = correct / len(data.validation_dataloader.dataset)
+        return val_loss, val_acc
+
+
 def train_model(parameters: Parameters, data: Data, training_model: TrainingModel):
-    for i, (images, labels) in enumerate(data.training_dataloader):
+    for batch, (images, labels) in enumerate(data.training_dataloader):
         images, labels = images.to(training_model.device), labels.to(training_model.device)
         for epoch in range(parameters.epochs):
-            training_model.model.train()
-            training_model.optimizer.zero_grad()
-            output = training_model.model(images)
-            loss = F.nll_loss(output, labels)
-            loss.backward()
-            training_model.optimizer.step()
+
+            loss = train_one_epoch(images, labels, training_model)
+
             print(
-                f"Train Epoch: {epoch} | Batch: {i}/{len(data.training_dataloader)} "
+                f"Train Epoch: {epoch} | Batch: {batch}/{len(data.training_dataloader)} "
                 f"| Loss: {loss.item():.4f}"
             )
-            # validate
-            val_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for images, labels in data.validation_dataloader:
-                    images, labels = images.to(training_model.device), labels.to(training_model.device)
-                    training_model.model.eval()
-                    output = training_model.model(images)
-                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(labels.view_as(pred)).sum().item()
-                val_loss /= len(data.validation_dataloader.dataset)
-                val_acc = correct / len(data.validation_dataloader.dataset)
 
-                print(f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}")
+            val_loss, val_acc = get_validation_loss(data, training_model)
 
+            print(f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}")
 
 
