4c79b1235dcf1309c2cfc839006636306b7a26e0
CDTAdmin <cdtadmin@ndmpc966.ndm.ox.ac.uk>
2022-09-30 11:22:28 +0100

    Update train.py and cli.py.

--- bin/cli.py
+++ bin/cli.py
@@ -7,6 +7,10 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 import typer
+from ser.model import modeldevice
+from ser.transforms import torchtransforms
+from ser.data import dataloader
+from ser.train import modeltrain
 
 main = typer.Typer()
 
@@ -35,88 +39,21 @@ def train(
     # save the parameters!
 
     # load model
-    model = Net().to(device)
+    model = modeldevice(device)
 
     # setup params
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     # torch transforms
-    ts = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
-    )
+    ts = torchtransforms()
 
     # dataloaders
-    training_dataloader = DataLoader(
-        datasets.MNIST(root="../data", download=True, train=True, transform=ts),
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=1,
-    )
-
-    validation_dataloader = DataLoader(
-        datasets.MNIST(root=DATA_DIR, download=True, train=False, transform=ts),
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=1,
-    )
+    training_dataloader = dataloader("../data", True, ts, batch_size)
+    validation_dataloader = dataloader(DATA_DIR, False, ts, batch_size)
 
     # train
-    for epoch in range(epochs):
-        for i, (images, labels) in enumerate(training_dataloader):
-            images, labels = images.to(device), labels.to(device)
-            model.train()
-            optimizer.zero_grad()
-            output = model(images)
-            loss = F.nll_loss(output, labels)
-            loss.backward()
-            optimizer.step()
-            print(
-                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
-                f"| Loss: {loss.item():.4f}"
-            )
-            # validate
-            val_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for images, labels in validation_dataloader:
-                    images, labels = images.to(device), labels.to(device)
-                    model.eval()
-                    output = model(images)
-                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(labels.view_as(pred)).sum().item()
-                val_loss /= len(validation_dataloader.dataset)
-                val_acc = correct / len(validation_dataloader.dataset)
+    modeltrain(epochs, device, model, optimizer, training_dataloader, validation_dataloader)
 
-                print(
-                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
-                )
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
 
 
 @main.command()
--- ser/train.py
+++ ser/train.py
@@ -1,56 +1,7 @@
-from pathlib import Path
 import torch
-from torch import optim
-import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 
-import typer
-from ser.model import modeldevice
-from ser.transforms import torchtransforms
-from ser.data import dataloader
-
-main = typer.Typer()
-
-PROJECT_ROOT = Path(__file__).parent.parent
-DATA_DIR = PROJECT_ROOT / "data"
-
-
-@main.command()
-def train(
-    name: str = typer.Option(
-        ..., "-n", "--name", help="Name of experiment to save under."
-    ),
-    epochs: int = typer.Option(
-        2, "-e", "--epochs", help="Number of epochs."
-    ),
-    batch_size: int = typer.Option(
-        1000, "-b", "--batch_size", help="Batch size."
-    ),
-    learning_rate: float = typer.Option(
-        0.01, "-l", "--learning_rate", help="Learning rate."
-    )
-):
-    print(f"Running experiment {name}")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # save the parameters!
-
-    # load model
-    model = modeldevice(device)
-
-    # setup params
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-
-    # torch transforms
-    ts = torchtransforms()
-
-    # dataloaders
-    training_dataloader = dataloader("../data", True, ts, batch_size)
-    validation_dataloader = dataloader(DATA_DIR, False, ts, batch_size)
-
-    # train
+def modeltrain(epochs, device, model, optimizer, training_dataloader, validation_dataloader):
     for epoch in range(epochs):
         for i, (images, labels) in enumerate(training_dataloader):
             images, labels = images.to(device), labels.to(device)
@@ -81,9 +32,3 @@ def train(
                 print(
                     f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
                 )
-
-
-
-@main.command()
-def infer():
-    print("This is where the inference code will go")
