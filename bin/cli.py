9e59ccf40626a5f4164efc7225a419d3ecb83bc4
robinyjpark <robinyjpark@github.com>
2022-09-30 10:03:13 +0100

    add typer options to supply parameters by CLI

--- bin/cli.py
+++ bin/cli.py
@@ -19,12 +19,19 @@ def train(
     name: str = typer.Option(
         ..., "-n", "--name", help="Name of experiment to save under."
     ),
+    epochs: int = typer.Option(
+        2, "-e", "--epochs", help="Number of epochs to train."
+    ),
+    batch_size: int = typer.Option(
+        1000, "-bs", "--batch-size", help="Number of samples in each training batch."
+    ),
+    learning_rate: float = typer.Option(
+        0.01, "-lr", "--learning-rate", help="Step size at each iteration."
+    ),
+
 ):
     print(f"Running experiment {name}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    epochs = 2
-    batch_size = 1000
-    learning_rate = 0.01
 
     # save the parameters!
 
