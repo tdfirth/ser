9bc5c1e85bd72abe02cce456d30a6fa2f7a537f7
robinyjpark <robinyjpark@github.com>
2022-09-30 10:19:23 +0100

    factorise model

--- bin/cli.py
+++ bin/cli.py
@@ -8,6 +8,8 @@ from torchvision import datasets, transforms
 
 import typer
 
+from ser.model import model_setup
+
 main = typer.Typer()
 
 PROJECT_ROOT = Path(__file__).parent.parent
@@ -35,11 +37,8 @@ def train(
 
     # save the parameters!
 
-    # load model
-    model = Net().to(device)
-
-    # setup params
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    # pull in model and optimizer
+    model, optimizer = model_setup(device, learning_rate)
 
     # torch transforms
     ts = transforms.Compose(
@@ -92,33 +91,6 @@ def train(
                 print(
                     f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
                 )
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
 
 @main.command()
 def infer():
