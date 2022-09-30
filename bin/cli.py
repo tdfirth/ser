66ab54ea96adf5ebb32e6e1782221529fe22e7fe
CDTAdmin <cdtadmin@ndmmac65.ndm.ox.ac.uk>
2022-09-30 10:43:17 +0100

    refactoring

--- bin/cli.py
+++ bin/cli.py
@@ -8,6 +8,15 @@ from torchvision import datasets, transforms
 
 import typer
 
+# importing model
+from ser.model import Net
+# importing transforms
+from ser.transforms import ts
+# importing data loaders
+from ser.data import training_dataloader, validation_dataloader
+# importing trainer
+from ser.train import trainer
+
 main = typer.Typer()
 
 PROJECT_ROOT = Path(__file__).parent.parent
@@ -28,14 +37,12 @@ def train(
         ..., "-b", "--batch", help="Batch size."
     ),
 
-    learning_rate: int = typer.Option(
+    learning_rate: float = typer.Option(
         ..., "-l", "--learning", help ="Learning rate."
     )
 ):
     print(f"Running experiment {name}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # save the parameters!
 
     # load model
     model = Net().to(device)
@@ -43,84 +50,38 @@ def train(
     # setup params
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    # torch transforms
-    ts = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
-    )
-
-    # dataloaders
-    training_dataloader = DataLoader(
-        datasets.MNIST(root="../data", download=True, train=True, transform=ts),
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=1,
-    )
-
-    validation_dataloader = DataLoader(
-        datasets.MNIST(root=DATA_DIR, download=True, train=False, transform=ts),
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=1,
-    )
-
     # train
-    for epoch in range(epochs):
-        for i, (images, labels) in enumerate(training_dataloader):
-            images, labels = images.to(device), labels.to(device)
-            model.train()
-            optimizer.zero_grad()
-            output = model(images)
-            loss = F.nll_loss(output, labels)
-            loss.backward()
-            optimizer.step()
-            print(
-                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
-                f"| Loss: {loss.item():.4f}"
-            )
-            # validate
-            val_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for images, labels in validation_dataloader:
-                    images, labels = images.to(device), labels.to(device)
-                    model.eval()
-                    output = model(images)
-                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(labels.view_as(pred)).sum().item()
-                val_loss /= len(validation_dataloader.dataset)
-                val_acc = correct / len(validation_dataloader.dataset)
+    # for epoch in range(epochs):
+    #     for i, (images, labels) in enumerate(training_dataloader):
+    #         images, labels = images.to(device), labels.to(device)
+    #         model.train()
+    #         optimizer.zero_grad()
+    #         output = model(images)
+    #         loss = F.nll_loss(output, labels)
+    #         loss.backward()
+    #         optimizer.step()
+    #         print(
+    #             f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
+    #             f"| Loss: {loss.item():.4f}"
+    #         )
+    #         # validate
+    #         val_loss = 0
+    #         correct = 0
+    #         with torch.no_grad():
+    #             for images, labels in validation_dataloader:
+    #                 images, labels = images.to(device), labels.to(device)
+    #                 model.eval()
+    #                 output = model(images)
+    #                 val_loss += F.nll_loss(output, labels, reduction="sum").item()
+    #                 pred = output.argmax(dim=1, keepdim=True)
+    #                 correct += pred.eq(labels.view_as(pred)).sum().item()
+    #             val_loss /= len(validation_dataloader.dataset)
+    #             val_acc = correct / len(validation_dataloader.dataset)
 
-                print(
-                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
-                )
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.log_softmax(x, dim=1)
-        return output
-
+    #             print(
+    #                 f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
+    #             )
+    trainer()
 
 @main.command()
 def infer():
