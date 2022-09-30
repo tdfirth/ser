0fe35b30f671062f2762bb9f86712c244344f613
CDTAdmin <cdtadmin@ndmpc966.ndm.ox.ac.uk>
2022-09-30 11:28:33 +0100

    Update data.py.

--- ser/data.py
+++ ser/data.py
@@ -7,6 +7,5 @@ def dataloader(root, train, transform, batch_size):
         datasets.MNIST(root=root, download=True, train=train, transform=transform),
         batch_size=batch_size,
         shuffle=train,
-        num_workers=1,
-    )
+        num_workers=1,)
 
