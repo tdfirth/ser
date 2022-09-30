fda011dc1b9acde2d3fb4c4ea8d99e8af0ee1eeb
CDTAdmin <cdtadmin@ndmpc779.ndm.ox.ac.uk>
2022-09-30 10:33:47 +0100

    add training function

--- ser/train.py
+++ ser/train.py
@@ -13,4 +13,34 @@ main = typer.Typer()
 PROJECT_ROOT = Path(__file__).parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 
-def train():
+def training(epochs, training_dataloader, validation_dataloader, model, device, optimizer):
+    for epoch in range(epochs):
+        for i, (images, labels) in enumerate(training_dataloader):
+            images, labels = images.to(device), labels.to(device)
+            model.train()
+            optimizer.zero_grad()
+            output = model(images)
+            loss = F.nll_loss(output, labels)
+            loss.backward()
+            optimizer.step()
+            print(
+                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
+                f"| Loss: {loss.item():.4f}"
+            )
+            # validate
+            val_loss = 0
+            correct = 0
+            with torch.no_grad():
+                for images, labels in validation_dataloader:
+                    images, labels = images.to(device), labels.to(device)
+                    model.eval()
+                    output = model(images)
+                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
+                    pred = output.argmax(dim=1, keepdim=True)
+                    correct += pred.eq(labels.view_as(pred)).sum().item()
+                val_loss /= len(validation_dataloader.dataset)
+                val_acc = correct / len(validation_dataloader.dataset)
+
+                print(
+                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
+                )
